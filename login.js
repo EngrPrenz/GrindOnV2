8fd221f42688e0323d192515882ea19e49ff511a
Yiizer <reingabrielgavino1723@gmail.com>
2025-04-13 13:29:36 +0800

    123

---
+++ login.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC5n40vPlIjXQ25X4NJlr8Z2jRGux0C1Y8",
+    authDomain: "grindon-da126.firebaseapp.com",
+    projectId: "grindon-da126",
+    storageBucket: "grindon-da126.firebasestorage.app",
+    messagingSenderId: "606558901364",
+    appId: "1:606558901364:web:e39156bea8d403f191ed21"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+//button
+const submit = document.getElementById('submit');
+submit.addEventListener("click", function (event){
+    event.preventDefault()
+
+    //inputs
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            alert("account created")
+            window.location.href = "home.html";
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage)
+            // ..
+        });
+
+})
